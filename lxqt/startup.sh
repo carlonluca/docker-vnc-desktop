57b2ffaa2587dd08d6b8609bcfbace11d56bdeb5
Luca Carlon <carlon.luca@gmail.com>
2023-02-20 18:18:35 +0100

    Added lxqt dockerfiles.

---
+++ lxqt/startup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [ -n "$VNC_PASSWORD" ]; then
+    echo -n "$VNC_PASSWORD" > /.password1
+    x11vnc -storepasswd $(cat /.password1) /.password2
+    chmod 400 /.password*
+    sed -i 's/^command=x11vnc.*/& -rfbauth \/.password2/' /etc/supervisor/conf.d/supervisord.conf
+    export VNC_PASSWORD=
+fi
+
+if [ -n "$X11VNC_ARGS" ]; then
+    sed -i "s/^command=x11vnc.*/& ${X11VNC_ARGS}/" /etc/supervisor/conf.d/supervisord.conf
+fi
+
+if [ -n "$OPENBOX_ARGS" ]; then
+    sed -i "s#^command=/usr/bin/openbox\$#& ${OPENBOX_ARGS}#" /etc/supervisor/conf.d/supervisord.conf
+fi
+
+if [ -n "$RESOLUTION" ]; then
+    sed -i "s/1024x768/$RESOLUTION/" /usr/local/bin/xvfb.sh
+fi
+
+USER=${USER:-root}
+HOME=/root
+if [ "$USER" != "root" ]; then
+    echo "* enable custom user: $USER"
+    useradd --create-home --shell /bin/bash --user-group --groups adm,sudo $USER
+    if [ -z "$PASSWORD" ]; then
+        echo "  set default password to \"ubuntu\""
+        PASSWORD=ubuntu
+    fi
+    HOME=/home/$USER
+    echo "$USER:$PASSWORD" | chpasswd
+    chown -R $USER:$USER ${HOME}
+    [ -d "/dev/snd" ] && chgrp -R adm /dev/snd
+fi
+sed -i -e "s|%USER%|$USER|" -e "s|%HOME%|$HOME|" /etc/supervisor/conf.d/supervisord.conf
+
+exec /usr/bin/tini -- supervisord -n -c /etc/supervisor/supervisord.conf
